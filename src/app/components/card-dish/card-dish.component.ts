05ca594b84d44c8a41f6068eabd7325bf1455973
Vladimir <mendozahum20@gmail.com>
2021-11-28 17:18:13 -0600

    feat: show alert when doesn't exist a category, ingredient or country

--- src/app/components/card-dish/card-dish.component.ts
+++ src/app/components/card-dish/card-dish.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { Meal } from 'src/app/interfaces/recipesBy.interface';
+import { AlertService } from 'src/app/services/alert.service';
 import { RecipesServiceService } from 'src/app/services/recipes-service.service';
 
 @Component({
@@ -20,7 +21,8 @@ export class CardDishComponent implements  OnChanges {
   recipes: Meal[] = [];
 
   constructor( private recipeService: RecipesServiceService,
-               private router: Router) { }
+               private router: Router,
+               private alertService: AlertService) { }
  
 
 
@@ -37,29 +39,48 @@ export class CardDishComponent implements  OnChanges {
     console.log( term, filter );
     switch ( filter ) {
       case 'category':
-        console.log('entre')
 
           this.recipeService.filterByCategory( term )
               .subscribe( recipes => {
 
-                console.log( recipes )
-                 this.recipes = recipes;
-                 console.log( this.recipes )
+                  if (recipes === null ) {
+                    this.alertService.showAlert('Oops', 'That category does not exist', 'info');
+                    this.router.navigateByUrl('/categories');
+                  } else {
+                    this.recipes = recipes;
+
+                  }
               });
         break;
       case 'ingredient':
           this.recipeService.filterByIngredient( term )
           .subscribe( recipes => {
-            this.recipes = recipes;
+
+             if (recipes === null ) {
+                  this.alertService.showAlert('Oops', 'That ingredient does not exist', 'info');
+                  this.router.navigateByUrl('/discover');
+              } else {
+                  this.recipes = recipes;
+
+              }
+              
           });
         break;
       case 'country':
           this.recipeService.filterByArea( term )
           .subscribe( recipes => {
-            this.recipes = recipes;
+            if (recipes === null ) {
+              this.alertService.showAlert('Oops', 'That country does not exist', 'info');
+              this.router.navigateByUrl('/discover');
+          } else {
+              this.recipes = recipes;
+
+          }
           });
         break;
+
       default:
+        this.router.navigateByUrl('/home');
         break;
     }
   }
--- src/app/pages/recipe/recipe.component.ts
+++ src/app/pages/recipe/recipe.component.ts
@@ -1,7 +1,8 @@
 import { isNull } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Meal } from 'src/app/interfaces/recipes.interface';
+import { AlertService } from 'src/app/services/alert.service';
 import { RecipesServiceService } from 'src/app/services/recipes-service.service';
 
 @Component({
@@ -20,7 +21,9 @@ export class RecipeComponent implements OnInit {
   showInstructions = false;
 
   constructor( private recipeService: RecipesServiceService,
-               private activatedRouter: ActivatedRoute) { }
+               private activatedRouter: ActivatedRoute,
+               private alertService: AlertService,
+               private router: Router) { }
 
   ngOnInit(): void {
 
@@ -31,15 +34,20 @@ export class RecipeComponent implements OnInit {
 
   getRecipe( id: string ) {
     this.recipeService.getRecipeById( id ).subscribe( recipe => {
-     
-      this.recipe = recipe;
 
+
+        this.recipe = recipe;
+        this.getIngredients( this.recipe );
+        this.getVideo( this.recipe.strYoutube );
+        this.getTags( this.recipe.strTags );
       
-      this.getIngredients( this.recipe );
-      this.getVideo( this.recipe.strYoutube );
-      this.getTags( this.recipe.strTags );
+     
+      
 
-    });
+    },( err => {
+        this.alertService.showAlert('Oops...', 'That recipe does not exist', 'info');
+        this.router.navigateByUrl('/home');
+    }));
   }
 
   getIngredients( recipe: any ) {
@@ -84,7 +92,6 @@ export class RecipeComponent implements OnInit {
 
   show( value: boolean ) {
     this.showI = value;
-    // this.showInstructions = this.showInstructions;
   }
 
 }
